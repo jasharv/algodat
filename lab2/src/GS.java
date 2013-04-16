ba9df3bbb6e7887da9124016c1c63c0c857710e7
Jashar Vahid <jasharv@gmail.com>
2013-04-16 20:05:03 +0200

    Stack overflow, men grafbyggande borde gå att göra typ så här.

--- lab2/src/GS.java
+++ lab2/src/GS.java
@@ -8,26 +8,51 @@ import java.util.TreeMap;
 import java.util.TreeSet;
 
 public class GS {
-	public static void main(String[] args) {	        
+	public static void main(String[] args) throws FileNotFoundException {
 		ArrayList<String> allaFiler = new ArrayList<String>();
 		ArrayList<Node> graph = new ArrayList<Node>();
-		allaFiler.add("testlab2/words-10-test");
-		allaFiler.add("testlab2/words-250-test");
-		allaFiler.add("testlab2/words-5757-test");
-		
-		for(String fileNames:allaFiler){
+		allaFiler.add("testlab2/words-10");
+		allaFiler.add("testlab2/words-250");
+		allaFiler.add("testlab2/words-5757");
 
-			Scanner scan = new Scanner(new File(fileNames + ".dat.txt"));
+		for (String fileNames : allaFiler) {
+
+			Scanner scan;
+			scan = new Scanner(new File(fileNames + ".dat.txt"));
 			ArrayList<String> words = new ArrayList<String>();
-			while(scan.hasNext()) {
+			while (scan.hasNext()) {
 				words.add(scan.nextLine());
 			}
-			for(String word: words){
-				graph.add(new Node(word));
-				
-				for(String otherWord: words){
-					if(otherWord.contains(word.substring(1,1))){
-						
+			for (String word : words) {							
+				Node node = null;
+				for (Node n : graph) {
+					node = n.getNodeIfContains(word);
+					if (node != null) {
+						break;
+					}
+				}
+				if (node == null) {
+					node = new Node(word);
+					graph.add(node);
+				}
+
+				for (String otherWord : words) {						// Compare word to all other words
+					if (otherWord.contains(word.substring(1, 1))
+							&& otherWord.contains(word.substring(2, 2))		//If contains the right letters (THIS IS WRONG, no check for multiples of one letter)
+							&& otherWord.contains(word.substring(3, 3))
+							&& otherWord.contains(word.substring(4, 4))
+							&& !(otherWord.equals(word))) {					// Not a match with itself
+						Node childNode = null;
+						for (Node n : graph) {
+							childNode = n.getNodeIfContains(otherWord);		//If matched word already exists in the graph, use it....
+							if (childNode != null) {
+								break;
+							}
+						}
+						if (childNode == null) {							// else create a node with that word.
+							childNode = new Node(otherWord);
+						}
+						node.children.add(childNode);						// add matched word as child
 					}
 				}
 			}
@@ -35,42 +60,46 @@ public class GS {
 			
 			
 			
-			
-			
-			
-			
-			
-		
-		try {
-			scan = new Scanner(new File(fileNames + ".out.txt"));
-		} catch (FileNotFoundException e) {
-			System.exit(1);
-		}
-		String[] outFile = new String[n];
-		int i = 0;
-		while(scan.hasNext()){
-			outFile[i] = (scan.nextLine());
-			i++;
-		}
-
-		Arrays.sort(outFile);
-		Arrays.sort(outPut);
-		boolean failed = false;
-		for (i = 0; i < n; i++){
-			System.out.println(outPut[i]);
-			if(!(outFile[i].equals(outPut[i]))){
-				
-				System.out.println("samst");
-				failed = true;
+			// BFS-ALGORITHM
+			try {
+				scan = new Scanner(new File(fileNames + "-test.in.txt"));
+			} catch (FileNotFoundException e1) {
+				System.exit(1);
+			}
+			ArrayList<String> outPut = new ArrayList<String>();
+			while (scan.hasNext()) {
+				outPut.add("HAR SKA AVSTANDET IN FOR VARJE ORDPAR");
 			}
 			
+			
+			
+			
+
+			try {
+				scan = new Scanner(new File(fileNames + "-test.out.txt"));
+			} catch (FileNotFoundException e) {
+				System.exit(1);
+			}
+			ArrayList<String> outFile = new ArrayList<String>();
+			int i = 0;
+			while (scan.hasNext()) {
+				outFile.add(scan.nextLine());
+			}
+			boolean failed = false;
+			for (i = 0; i < outFile.size(); i++) {
+				System.out.println(outPut.get(i));
+				if (!(outFile.get(i).equals(outFile.get(i)))) {
+
+					System.out.println("samst");
+					failed = true;
+				}
+			}
+			if (!failed) {
+				System.out.println("Det stammer javligt bra \n");
+			}
+
 		}
-		if(!failed){
-		System.out.println("Det stammer javligt bra \n");
-		}
-     
-        }
-		 
+
 	}
-	
+
 }
--- lab2/src/Node.java
+++ lab2/src/Node.java
@@ -1,11 +1,28 @@
 import java.util.ArrayList;
 
-
 public class Node {
 	String word;
 	public ArrayList<Node> children;
+
 	public Node(String word) {
-		this.word=word;
+		this.word = word;
 		children = new ArrayList<Node>();
+	}
+
+	public Node getNodeIfContains(String child) {
+		if (word.equals(child)){
+			return this;			// Om jag ar sokt, returnera mig
+		}
+		for (Node s : children) {
+			Node node = s.getNodeIfContains(child); //fortsatt leta
+			if (node!=null){
+				return node; // Om hittad
+			}
+		}
+		return null; //finns inte har
+	}
+
+	public String getWord() {
+		return word;
 	}
 }
